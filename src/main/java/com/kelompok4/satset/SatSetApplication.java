f76b84741b31ff64c8803dc70932a71fbe8293ce
Rhyred <robigold9@gmail.com>
2026-05-10 20:05:26 -0400

    update Init

---
+++ src/main/java/com/kelompok4/satset/SatSetApplication.java
@@ -0,0 +1,13 @@
+package com.kelompok4.satset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatSetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatSetApplication.class, args);
+	}
+
+}
