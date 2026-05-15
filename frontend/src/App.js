2e79eab185e7fa5da4e5a70ef295eb9c9ba834c8
kelvinmanuel6502-cloud <kelvinmanuel6502@gmail.com>
2026-05-15 09:42:34 +0700

    Add frontend react

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>✅ AKHIRNYA JALAN!</h1>
+      <p>Tugas SatSet Selesai Tepat Waktu.</p>
+    </div>
+  );
+}
+export default App;
