4f38de2e4759dfd81dbbe30b573a8e51f4cd69f5
Rhyred <robigold9@gmail.com>
2026-05-18 12:18:05 -0400

    remove wrong Push

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>✅ AKHIRNYA JALAN!</h1>
-      <p>Tugas SatSet Selesai Tepat Waktu.</p>
-    </div>
-  );
-}
-export default App;
