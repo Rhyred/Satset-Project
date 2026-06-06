c123de68ce8f7f3eb0a2e377c0621fa1fcc131e9
Rhyred <robigold9@gmail.com>
2026-06-06 01:37:27 -0400

    update-2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+
+    allowedHosts: [
+      'satset-superapp.rhyred.net'
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
